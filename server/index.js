c19248b9394ef8104f241b87b92fa44ea39a06bf
Himanshu sahu <hsahu8268@gmail.com>
2025-01-05 13:47:40 +0530

    Modified

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://study-notion-8wrn.onrender.com/",
+		origin:"http://localhost:3000",
 		credentials:true,
 	})
 )
